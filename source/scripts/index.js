9079a601c7a0955a6a89665757f3af286c22c5fd
Maria Frolova <maria.frolova15@gmail.com>
2024-12-06 03:52:31 +0300

    Добавляет mobile header, скрипт открытия/закрытия в header, состояния интерактивных элементов и карту

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,25 @@
 /* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-navigation');
+const navToggle = document.querySelector('.main-header__toggle');
+
+navMain.classList.remove('main-navigation--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-navigation--opened')) {
+    navMain.classList.remove('main-navigation--opened');
+    navMain.classList.add('main-navigation');
+  } else {
+    navMain.classList.add('main-navigation--opened');
+    navMain.classList.remove('main-navigation');
+  }
+});
+navMain.classList.remove('main-header__toggle--nojs');
+navToggle.addEventListener('click', () => {
+  if (navToggle.classList.contains('main-header__toggle--close')) {
+    navToggle.classList.remove('main-header__toggle--close');
+    navToggle.classList.add('main-header__toggle--open');
+  } else {
+    navToggle.classList.add('main-header__toggle--close');
+    navToggle.classList.remove('main-header__toggle--open');
+  }
+});
