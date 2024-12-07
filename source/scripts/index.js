8556623f586c11fd506d062bc94c9706d877b76b
Maria Frolova <maria.frolova15@gmail.com>
2024-12-07 18:59:00 +0300

    Правит в блоке header разметку, стили, скрипт

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,25 +1,8 @@
 /* в этот файл добавляет скрипты*/
-const navMain = document.querySelector('.main-navigation');
-const navToggle = document.querySelector('.main-header__toggle');
+const header = document.querySelector('.main-header');
+const burgerButton = document.querySelector('.main-header__toggle');
 
-navMain.classList.remove('main-navigation--nojs');
-
-navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-navigation--opened')) {
-    navMain.classList.remove('main-navigation--opened');
-    navMain.classList.add('main-navigation');
-  } else {
-    navMain.classList.add('main-navigation--opened');
-    navMain.classList.remove('main-navigation');
-  }
-});
-navMain.classList.remove('main-header__toggle--nojs');
-navToggle.addEventListener('click', () => {
-  if (navToggle.classList.contains('main-header__toggle--close')) {
-    navToggle.classList.remove('main-header__toggle--close');
-    navToggle.classList.add('main-header__toggle--open');
-  } else {
-    navToggle.classList.add('main-header__toggle--close');
-    navToggle.classList.remove('main-header__toggle--open');
-  }
+header.classList.remove ('main-header--nojs');
+burgerButton.addEventListener('click', () => {
+  header.classList.toggle ('main-header--opened');
 });
